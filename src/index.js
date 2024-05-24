2efe8b4866c4fa1b2f1de14a1d97d88af5de165a
Vaishnavi1601 <vaishnavijaiswal.18cs@saividya.ac.in>
2024-05-25 00:27:29 +0530

    added index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+/*
+1. ReactDOM.render method is used to render the App component into the DOM element with the ID root.
+
+2. <React.StrictMode> is a wrapper that helps with identifying potential problems in the application. 
+It activates additional checks and warnings for its descendants. */
+ReactDOM.render(
+  <React.StrictMode>       
+    <App/>
+  </React.StrictMode>,
+    document.getElementById('root')
+)
